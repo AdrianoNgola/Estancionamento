b6c1f45f271b035db55dc91527ca4428ebcca341
ALBANO <ALBANO@PC>
2024-11-22 21:09:08 +0000

    Add project files.

---
+++ ConsoleApp2/Estancionamento.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Estancionamento
+    {
+        private decimal PrecoInicial = 0;
+        private decimal PrecoPorHora = 0;
+        List<string> Veiculos = new List<string>();
+
+        public Estancionamento(decimal PrecoInic, decimal PrecoPorhora)
+        {
+            PrecoInicial = PrecoInic;
+            PrecoPorHora = PrecoPorhora;
+        }
+
+       
+        public void AdicionarVeiculo()
+        {
+            int contador = 0;
+            
+            Console.Write("Digite a placa do seu veículo: ");
+            string placa = Console.ReadLine();
+
+            Veiculos.Add(placa);
+
+            
+            contador++;
+        }
+
+        public void RemoverVeiculo()
+        {
+
+            string placa = "";
+
+            Console.Write("Digite a placa do seu veículo: ");
+            placa = Console.ReadLine();
+
+            if (Veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            {
+                int hora = 0;
+                Console.Write("Digite o tempo de estancionamento: ");
+                hora = Convert.ToInt32(Console.ReadLine());
+
+                decimal ValorTotal = PrecoInicial + PrecoPorHora * hora;
+
+                Veiculos.Remove(placa);
+
+                Console.WriteLine($"O veículo {placa} foi removido do estancionamento e o preço total foi de {ValorTotal}");
+
+            }
+            else
+            {
+                Console.WriteLine($"Desculpa, mas não foi encontrado o veículo {placa}, verifica se digitou corretamento a placa do seu veículo");
+            }
+            
+
+            
+
+
+
+
+
+
+
+        }
+
+        public void ListarVeiculos()
+        {
+            if (Veiculos.Any())
+            {
+                int contador = 0;
+                foreach (var item in Veiculos)
+                {
+                    Console.WriteLine("Os veículos estacionados são:");
+                    Console.WriteLine("=================");
+                    Console.WriteLine(Veiculos[contador]);
+
+                    contador++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Não há veículos estancionados");
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,68 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+namespace ConsoleApp2
+{
+    class ConsoleApp2
+    {
+        static void Main(string [] args)
+        {
+
+            Console.WriteLine("Seja bem-vindo ao sistema de gestão de estacionamento");
+            Console.WriteLine("============================");
+            Console.Write("Digite o preço inicial: ");
+            decimal Inicial = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("============================");
+            Console.Write("Digite o preço por hora: ");
+            decimal Hora = Convert.ToDecimal(Console.ReadLine());
+
+
+            Estancionamento Station = new Estancionamento(Inicial, Hora);
+
+           // string Opcoes = string.Empty;
+            bool ExiberMenu = true;
+
+            while (ExiberMenu)
+            {
+                Console.Clear();
+
+                Console.WriteLine("============================");
+                Console.WriteLine("Digite a sua opção");
+                Console.WriteLine("============================");
+                Console.WriteLine("1 --> Cadastrar veículo");
+                Console.WriteLine("2 --> Remover veículo");
+                Console.WriteLine("3 --> Consultar veículo");
+                Console.WriteLine("4 --> Encerrar");
+                Console.WriteLine("============================");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        Station.AdicionarVeiculo();
+                        break;
+
+                    case "2":
+                        Station.RemoverVeiculo();
+                        break;
+
+                    case "3":
+                        Station.ListarVeiculos();
+                        break;
+
+                    case "4":
+                        ExiberMenu = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção invalida!");
+                        break;
+                }
+
+                Console.WriteLine("Precione qualquer tecla para continuar");
+
+               
+            }
+
+        }
+    }
+}
